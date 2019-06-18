6868c28d73dc8ef098e35d3f1454a41a9e7caf8f
Vagner <vagner.alves.pavani@gmail.com>
2019-06-18 00:43:27 -0300

    terminado porem com bug em soma adjacentes

--- interface.c
+++ interface.c
@@ -12,7 +12,7 @@ void margem(void){
 void mostraOpcoesMenu(void){
     margem(); printf("--> Digite 1 para carregar matriz do teclado\n");
     margem(); printf("--> Digite 2 para carregar matriz do arquivo binario \n");
-    margem(); printf("--> Digite 3 para imprimir uma matriz na tela\n");
+    margem(); printf("--> Digite 3 para imprimir matriz\n");
     margem(); printf("--> Digite 4 para salvar uma matriz em um arquivo binario\n");
     margem(); printf("--> Digite 5 para alterar um elemento da matriz\n");
     margem(); printf("--> Digite 6 para somar duas matrizes\n");
@@ -22,7 +22,9 @@ void mostraOpcoesMenu(void){
     margem(); printf("--> Digite 10 para somar os elementos adjacentes de um dado elemento da matriz\n");
     margem(); printf("--> Digite 11 para terminar o programa \n\n");
 }
-void PrintMenu(){
+
+//funcao para mostrar o titulo grande.
+void printHeader(){
     int c;
     FILE * menuFilePointer;
     menuFilePointer = fopen("menu.txt", "r");
@@ -31,7 +33,6 @@ void PrintMenu(){
         printf("erro ao carregar arquivo\n");
         exit(1);
     }
-
     system("clear");
     while((c = fgetc(menuFilePointer)) != EOF){
         printf("%c", c);
@@ -58,19 +59,21 @@ int continua(){
 
 
 int main(){
-    int menuEscolha, lin, col;
-    float * matrizPointer;
+    int menuEscolha, lin, col, posicaoLin, posicaoCol, auxLin, auxCol;
+    float * matrizPointer, * auxMatrizPointer, *soma, elemento;
     FILE * matrizFilePointer;
     char nomeArquivo[20];
+    matrizPointer = NULL;
+    soma = (float *) malloc(sizeof(float));
 
     do{
-        PrintMenu();
+        printHeader();
         mostraOpcoesMenu();
         scanf("%d", &menuEscolha);
         
         switch(menuEscolha){
             case 1:
-                PrintMenu();
+                printHeader();
                 printf("Insira a dimensao da matriz:[lin] [col]:");
                 scanf("%d %d", &lin, &col);
                 matrizPointer = criaMatriz(lin, col);
@@ -81,7 +84,7 @@ int main(){
                 continua();
                 break;
             case 2:
-                PrintMenu();
+                printHeader();
                 printf("insira o nome do arquivo:");
                 scanf("%s", nomeArquivo);
                 printf("insira as dimensoes da matriz de destino:[lin] [col]:");
@@ -93,10 +96,16 @@ int main(){
                 imprimeMatriz( matrizPointer, lin, col);
                 continua();
                 break;
-
-            //case 3://
+            case 3:
+                if(matrizPointer != NULL){
+                    imprimeMatriz(matrizPointer, lin, col);
+                }else{
+                    printf("Nenhuma matriz no programa...\n");
+                }
+                continua();
+                break;
             case 4:
-                PrintMenu();
+                printHeader();
                 printf("Insira a dimensao da matriz:[lin] [col]:");
                 scanf("%d %d", &lin, &col);
                 matrizPointer = criaMatriz(lin, col);
@@ -108,12 +117,92 @@ int main(){
                 imprimeMatriz(matrizPointer,lin, col);
                 continua();
                 break;
-            /*case 5:
+            case 5:
+                printHeader();
+                if(matrizPointer != NULL){
+                    printf("Insira a posicao do elemento que deseja alterar:[lin] [col]:");
+                    scanf("%d %d", &posicaoLin, &posicaoCol);
+                    printf("Insira o novo valor:");
+                    scanf("%f", &elemento);
+                    alteraElementoMatriz(matrizPointer, lin, col, posicaoLin, posicaoCol, elemento);
+                    imprimeMatriz(matrizPointer, lin, col);
+                }else{
+                    printf("Nenhuma matriz no programa...");
+                }
+                continua();
+                break;
             case 6:
+                printHeader();
+                printf("Insira a dimensao das matrizes:[lin] [col]:");
+                scanf("%d %d", &lin , &col);
+                matrizPointer = criaMatriz(lin, col);
+                auxMatrizPointer = criaMatriz(lin, col);
+                printf("preencha a primeira matriz a ser somada:\n");
+                carregaMatrizTeclado(matrizPointer, lin, col);
+                printf("preencha a segunda matriz a ser somada:\n");
+                carregaMatrizTeclado(auxMatrizPointer, lin, col);
+                matrizPointer = somaMatrizes(matrizPointer, auxMatrizPointer, lin, col);
+                printf("O resultado eh:\n");
+                imprimeMatriz(matrizPointer, lin, col);
+                continua();
+                break;
             case 7:
+                printHeader();
+                printf("Insira a dimensao da primeira matriz:[lin] [col]:");
+                scanf("%d %d",&lin, &col);
+                printf("Insira a dimensao da segunda matriz:[lin] [col]:");
+                scanf("%d %d",&auxLin, &auxCol);
+                matrizPointer = criaMatriz(lin, col);
+                auxMatrizPointer = criaMatriz(auxLin, auxCol);
+                printf("Preencha primeira matriz:\n");
+                carregaMatrizTeclado(matrizPointer, lin, col);
+                printf("Preencha segunda matriz:\n");
+                carregaMatrizTeclado(auxMatrizPointer, auxLin, auxCol);
+                matrizPointer = multiplicaMatrizes (matrizPointer,auxMatrizPointer, lin, col, auxCol);
+                printf("O resultado eh:\n");
+                imprimeMatriz(matrizPointer, lin, col);
+                continua();
+                break;
             case 8:
+                printHeader();
+                if(matrizPointer != NULL){
+                    matrizPointer = transpostaMatriz(matrizPointer, lin, col);
+                    printf("A matriz transposta eh:\n");
+                    imprimeMatriz(matrizPointer, lin, col);
+                }else{
+                    printf("nenhuma matriz no programa...\n");
+                }
+                continua();
+                break;
             case 9:
-            case 10:*/
+                printHeader();
+                if(matrizPointer != NULL){
+                    if(ehMatrizIdentidade(matrizPointer, lin,col)){
+                        printf("eh identidade\n");
+                    }else if(ehMatrizTriangularSuperior(matrizPointer, lin, col)){
+                        printf("eh tringular superior\n");
+                    }else if(ehMatrizTriangularInferior(matrizPointer,lin,col)){
+                        printf("eh triangular inferior\n");
+                    }else{
+                        printf("nao eh tringular nem identidade.\n");
+                    }
+                }else{
+                    printf("Nenhuma matriz no programa...\n");
+                }
+                continua();
+                break;
+            case 10:
+                printHeader();
+                if(matrizPointer != NULL){
+                    printf("Insira a posicao do elemento para soma:[lin] [col]:");
+                    scanf("%d %d",&posicaoLin, &posicaoCol);
+                    somaAdjacentesElementoMatriz (matrizPointer, lin, col, posicaoLin, posicaoCol, soma);
+                    printf("A soma eh: ");
+                    imprimeMatriz(auxMatrizPointer, 1, 1);
+                }else{
+                    printf("Nenhuma matriz no programa...\n");
+                }
+                continua();
         }
     }while (menuEscolha != 11);
     return 0;
